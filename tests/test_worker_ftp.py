725b2f75ae8291f360cf0a4a2679f7ec4a22ef5b
klinga <klingaroo@gmail.com>
2021-04-08 15:33:01 -0400

    init test

--- tests/test_worker_ftp.py
+++ tests/test_worker_ftp.py
@@ -4,3 +4,14 @@ Tests ftp worker module
 import os
 
 import pytest
+
+
+from bookops_watchdog.worker_ftp import APP_FTP, ftp_connect
+
+
+def test_temp():
+    ftp = ftp_connect(**APP_FTP, library="bpl")
+    print(type(ftp))
+    print(ftp.retrlines("LIST"))
+
+    ftp.quit()
