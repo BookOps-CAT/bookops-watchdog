4e4c2a21a33850ff81b0d05e1cbaf6daf77f8a06
klinga <klingaroo@gmail.com>
2021-07-19 09:40:58 -0400

    basic ftp methods

--- tests/test_worker_ftp.py
+++ tests/test_worker_ftp.py
@@ -3,61 +3,80 @@ Tests ftp worker module
 """
 import ftplib
 import os
+from datetime import datetime, timedelta
 
 import pytest
 
 
-from bookops_watchdog.errors import WatchdogError
 from bookops_watchdog.worker_ftp import (
-    get_ftp_creds_from_file,
+    find_aged_out_files,
+    find_unprocessed_files,
     ftp_connect,
-    put_creds_to_env_var,
+    get_ftp_creds_from_env_var,
+    get_files_in_dir,
+    get_file_date,
+    get_library_directory,
+    is_aged_out,
 )
 
 
-@pytest.mark.local_only
-def test_get_ftp_creds_from_file(mock_local_ftp_creds):
-    creds_fh = os.path.join(os.environ["HOME"], ".ftp\\bwatch-ftp-cred.json")
-    assert get_ftp_creds_from_file(creds_fh) == {
-        "FTP_HOST": "foo",
-        "FTP_USER": "bar",
-        "FTP_PASSW": "spam",
-        "FTP_DIR": "baz",
-    }
+def test_get_ftp_creds_from_env_var(mock_all_env_variables):
 
-
-@pytest.mark.local_only
-def test_get_fpt_creds_from_file_file_not_found():
-    with pytest.raises(WatchdogError) as exc:
-        get_ftp_creds_from_file("foo.json")
-        assert (
-            str(exc)
-            == "FTP configuration error: unable to locate credentials at foo.json ."
-        )
-
-
-@pytest.mark.local_only
-def test_put_local_creds_to_env_var(mock_local_ftp_creds):
-    put_creds_to_env_var("local")
-    creds = [
-        os.getenv("FTP_HOST"),
-        os.getenv("FTP_USER"),
-        os.getenv("FTP_PASSW"),
-        os.getenv("FTP_DIR"),
-    ]
-    assert creds == ["foo", "bar", "spam", "baz"]
-
-
-@pytest.mark.parametrize("arg", ["BPL", "NYPL"])
-@pytest.mark.local_only
-def test_ftp_connect_local(arg):
-    creds_fh = os.path.join(os.environ["HOME"], ".ftp\\bwatch-ftp-cred.json")
-    creds = get_ftp_creds_from_file(creds_fh)
-    ftp = ftp_connect(
-        host=creds["FTP_HOST"],
-        user=creds["FTP_USER"],
-        passw=creds["FTP_PASSW"],
-        folder=creds["FTP_DIR"],
-        library=arg,
+    assert get_ftp_creds_from_env_var() == dict(
+        host="ftp.foo.com", user="fakeFtpUser", passw="fakeFtpPassw", folder="SPAM"
     )
+
+
+@pytest.mark.webtest
+def test_ftp_connect_live(live_dev_ftp_creds):
+    creds = live_dev_ftp_creds
+    ftp = ftp_connect(**creds, library="BPL")
     assert type(ftp) == ftplib.FTP
+
+
+@pytest.mark.webtest
+def test_get_files_in_dir_live(live_dev_ftp_creds):
+    creds = live_dev_ftp_creds
+    ftp = ftp_connect(**creds, library="BPL")
+    assert get_files_in_dir(ftp) == [
+        "BookOpsQC.20210707063001",
+        "BookOpsQC.20210709063000",
+    ]
+
+
+def test_get_library_directory():
+    assert get_library_directory("foo", "bar") == "foo/bar"
+
+
+def test_find_unprocessed_files():
+    retrieved = ["1.txt", "2.txt", "3.txt"]
+    processed = ["1.txt", "3.txt"]
+    assert find_unprocessed_files(retrieved, processed) == ["2.txt"]
+
+
+def test_get_file_date():
+    assert get_file_date("BookOpsQC.20210102131501") == datetime(
+        year=2021, month=1, day=2, hour=13, minute=15, second=1
+    )
+
+
+@pytest.mark.parametrize(
+    "arg1,arg2,expectation",
+    [
+        (datetime.now(), 1, False),
+        (datetime.now() - timedelta(days=1), 2, False),
+        (datetime.now() - timedelta(days=5), 4, True),
+        (datetime.now() - timedelta(days=31), 30, True),
+    ],
+)
+def test_is_aged_out(arg1, arg2, expectation):
+    assert is_aged_out(file_date=arg1, days=arg2) == expectation
+
+
+def test_find_aged_out_files():
+    files = [
+        f"BookOpsQC.{datetime.now():%Y%m%d%H%M%S}",
+        f"BookOpsQC.{datetime.now() - timedelta(days=20):%Y%m%d%H%S}",
+        f"BookOpsQC.{datetime.now() - timedelta(days=31):%Y%m%d%H%S}",
+    ]
+    assert find_aged_out_files(files=files, days=30) == [files[2]]
