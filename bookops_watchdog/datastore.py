6cdc5c182cdb08df48380fae45113f1be4545934
klinga <klingaroo@gmail.com>
2021-07-20 14:33:34 -0400

    datastore init

---
+++ bookops_watchdog/datastore.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+
+"""
+Watchdog's database models
+"""
+from datetime import datetime
+import os
+from contextlib import contextmanager
+
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    create_engine,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.schema import UniqueConstraint
+
+
+Base = declarative_base()
+
+
+class DataAccessLayer:
+    def __init__(self):
+        self.conn = f"sqlite:///{os.getenv('watchdog_store')}"
+        self.engine = None
+        self.session = None
+
+    def connect(self):
+        self.engine = create_engine(self.conn)
+        Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(bind=self.engine)
+
+
+dal = DataAccessLayer()
+
+
+@contextmanager
+def session_scope():
+    dal.connect()
+    session = dal.Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+class File(Base):
+    __tablename__ = "file"
+    wid = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, nullable=False, default=datetime.now())
+    handle = Column(String, nullable=False)
+    library_wid = Column(Integer, ForeignKey("library.wid"), nullable=False)
+
+    def __repr__(self):
+        return (
+            f"<File(wid='{self.wid}', timestamp='{self.timestamp}', "
+            f"handle='{self.handle}', library_wid='{self.library_wid}')>"
+        )
+
+
+class Library(Base):
+    __tablename__ = "library"
+
+    wid = Column(Integer, primary_key=True)
+    code = Column(String(3))
+
+    def __repr__(self):
+        return f"<Library(wid='{self.wid}', code='{self.code}')>"
+
+
+class Bib(Base):
+    __tablename__ = "bib"
+    __table_arg__ = UniqueConstraint("wid", "library_wid")
+
+    wid = Column(Integer, primary_key=True, autoincrement=False)
+    library_wid = Column(Integer, ForeignKey("library.wid"), nullable=False)
+    bibType = Column(String(1))
+    catDate = Column(DateTime)
+    title = Column(String(25), nullable=False)
+    author = Column(String(25))
+    callNo = Column(String(100))
+    callFormat = Column(String(5), nullable=False)
+    callAudn = Column(String(1), nullable=False)
+    callWl = Column(Boolean, nullable=False)
+    callType = Column(String(5))
+    callCutter = Column(Boolean, nullable=False)
+    callDewey = Column(String)
+    allowedBplDiv = Column(Integer)
+    subjects = Column(String)
+    subjectPerson = Column(Boolean, nullable=False)
+    critWork = Column(Boolean, nullable=False)
+
+    orders = relationship("Order", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return (
+            f"<Bib(wid='{self.wid}', libary_wid='{self.library_wid}', "
+            f"bibType='{self.bibType}', catDate='{self.catDate}', "
+            f"title='{self.title}')>, "
+            f"author='{self.author}', callNo='{self.callNo}', "
+            f"callFormat='{self.callFormat}', "
+            f"callAudn='{self.callAudn}', callWl='{self.callWl}', "
+            f"callType='{self.callType}', "
+            f"callCutter='{self.callCutter}', callDewey='{self.callDewey}', "
+            f"allowedBplDiv='{self.allowedBplDiv}', subjects='{self.subjects}', "
+            f"subjectPerson='{self.subjectPerson}', critWork='{self.critWork}')>"
+        )
+
+
+class Order(Base):
+    __tablename__ = "order"
+    wid = Column(Integer, primary_key=True, autoincrement=False)
+    bib_wid = Column(Integer, ForeignKey("bib.wid"), nullable=False)
+    orderDate = Column(DateTime)
+    orderBranches = Column(String)
+    orderShelves = Column(String)
+    orderAudn = Column(String(1))
+    copies = Column(Integer, nullable=False)
+    venNote = Column(String)
+
+    def __repr__(self):
+        return (
+            f"<Order(wid='{self.wid}', bib_wid='{self.bib_wid}', "
+            f"orderDate='{self.orderDate}', orderBranches='{self.orderBranches}', "
+            f"orderShelves='{self.orderShelves}', orderAudn='{self.orderAudn}', "
+            f"copies='{self.copies}', venNote='{self.venNote}')>"
+        )
+
+
+class Conflict(Base):
+    __tablename__ = "conflict"
+    wid = Column(Integer, primary_key=True)
+    tier = Column(String(7))
+    code = Column(String(8))
+    description = Column(String)
+
+    def __repr__(self):
+        return (
+            f"<Conflict(wid='{self.wid}', tier='{self.tier}', "
+            f"code='{self.code}', description='{self.description}')>"
+        )
+
+
+class Ticket(Base):
+    __tablename__ = "ticket"
+    wid = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, nullable=False, default=datetime.now())
+    conflict_wid = Column(Integer, ForeignKey("conflict.wid"), nullable=False)
+    order_wid = Column(Integer, nullable=False)
+    bib_wid = Column(Integer, nullable=False)
+    copies = Column(Integer, nullable=False, default=0)
+    reported = Column(Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return (
+            f"<Ticket(wid='{self.wid}', timestamp='{self.timestamp}', "
+            f"conflict_wid='{self.conflict_wid}', "
+            f"order_wid='{self.order_wid}', bib_wid='{self.bib_wid}', "
+            f"copies='{self.copies}', reported='{self.reported}')>"
+        )
---
+++ tests/test_datastore.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from bookops_watchdog.datastore import (
+    Bib,
+    Conflict,
+    DataAccessLayer,
+    File,
+    Library,
+    Order,
+    Ticket,
+)
+
+
+def test_bib_tbl_repr():
+    bib = Bib(
+        library_wid=1,
+        bibType="b",
+        catDate="2021-07-01",
+        title="Foo",
+        author="Bar",
+        callNo="ABC",
+        callFormat="print",
+        callAudn="j",
+        callWl=False,
+        callType="dew",
+        callCutter=True,
+        callDewey="111",
+        allowedBplDiv=16,
+        subjects="foo-bar",
+        subjectPerson=False,
+        critWork=False,
+    )
+    assert (
+        str(bib)
+        == "<Bib(wid='None', libary_wid='1', bibType='b', catDate='2021-07-01', title='Foo')>, author='Bar', callNo='ABC', callFormat='print', callAudn='j', callWl='False', callType='dew', callCutter='True', callDewey='111', allowedBplDiv='16', subjects='foo-bar', subjectPerson='False', critWork='False')>"
+    )
+
+
+def test_conflict_tbl_repr():
+    conflict = Conflict(wid=1, tier="Foo", code="bar", description="spam")
+    assert (
+        str(conflict)
+        == "<Conflict(wid='1', tier='Foo', code='bar', description='spam')>"
+    )
+
+
+def test_DataAccessLayer_conn(mock_all_env_variables):
+    assert (
+        DataAccessLayer().conn
+        == "sqlite:///C:\\Users\\Foo\\APPDATA\\TEMP\\Bookops-Watchdog\\datastore.db"
+    )
+
+
+def test_file_tbl_repr():
+    file = File(wid=1, timestamp="2021-07-01-07:01", handle="foo.txt", library_wid=1)
+    assert (
+        str(file)
+        == "<File(wid='1', timestamp='2021-07-01-07:01', handle='foo.txt', library_wid='1')>"
+    )
+
+
+def test_library_tbl_repr():
+    assert str(Library(wid=1, code="foo")) == "<Library(wid='1', code='foo')>"
+
+
+def test_order_tbl_repr():
+    order = Order(
+        bib_wid=1,
+        orderDate="2021-07-01",
+        orderBranches="41,50,16",
+        orderShelves="afc",
+        orderAudn="a",
+        copies=20,
+        venNote="nfc",
+    )
+    assert (
+        str(order)
+        == "<Order(wid='None', bib_wid='1', orderDate='2021-07-01', orderBranches='41,50,16', orderShelves='afc', orderAudn='a', copies='20', venNote='nfc')>"
+    )
+
+
+def test_ticket_tbl_repr():
+    issue = Ticket(
+        wid=1,
+        timestamp="2021-07-01-07:01",
+        conflict_wid=2,
+        order_wid=3,
+        bib_wid=4,
+        copies=10,
+        reported=False,
+    )
+    assert (
+        str(issue)
+        == "<Ticket(wid='1', timestamp='2021-07-01-07:01', conflict_wid='2', order_wid='3', bib_wid='4', copies='10', reported='False')>"
+    )
